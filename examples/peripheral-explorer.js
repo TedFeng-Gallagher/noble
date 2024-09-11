9677ecd06cdeffd0bd1e39fadbee17fe3b2b22d9
Ted Feng <Xigang.Feng@gallagher.com>
2024-09-12 11:52:08 +1200

    Update noble examples to test HR0 BLE.

--- examples/peripheral-explorer.js
+++ examples/peripheral-explorer.js
@@ -1,8 +1,10 @@
 /* eslint-disable handle-callback-err */
 const async = require('async');
-const noble = require('../index');
+// const noble = require('../index');
+const noble = require('@abandonware/noble');
 
-const peripheralIdOrAddress = process.argv[2].toLowerCase();
+// const peripheralIdOrAddress = process.argv[2].toLowerCase();
+const peripheralIdOrAddress = "ac:67:b2:71:63:6a"
 
 noble.on('stateChange', function (state) {
   if (state === 'poweredOn') {
